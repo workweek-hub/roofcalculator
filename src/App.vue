351a2027e771e78814da3cfe7920b73b40e9cc78
Hard Alfred <alfred@weekwork.ml>
2021-10-21 01:03:36 +0300

    Added choice of roof and corrugated board

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,89 @@
 <template>
-  <h1>Калькулятор профнастила на крышу</h1>
+  <div class="m-8">
+    <h1 class="text-3xl font-bold text-gray-700 mb-8">
+      Калькулятор профнастила на крышу
+    </h1>
+    <item-selection
+      :type="'roofs'"
+      :section="'Выберите тип кровли'"
+      :sectionData="roof"
+      @select-item="selectItem"
+    />
+    <item-selection
+      :type="'boards'"
+      :section="'Тип профнастила'"
+      :sectionData="corrugatedBoard"
+      @select-item="selectItem"
+    />
+  </div>
 </template>
 
 <script>
+import itemSelection from "@/components/itemSelection";
 export default {
   name: "App",
+  components: {
+    itemSelection,
+  },
+  data() {
+    return {
+      roof: [
+        {
+          id: "singleSlope",
+          ruTitle: "Односкатная",
+        },
+        {
+          id: "gable",
+          ruTitle: "Двускатная",
+        },
+        {
+          id: "valm",
+          ruTitle: "Вальмовая",
+        },
+        {
+          id: "hipRoof",
+          ruTitle: "Шатровая",
+        },
+        {
+          id: "attic",
+          ruTitle: "Мансардная",
+        },
+      ],
+      selectedRoof: null,
+      corrugatedBoard: [
+        {
+          id: "C20",
+          ruTitle: "Профнастил С20",
+        },
+        {
+          id: "C21",
+          ruTitle: "Профнастил С21",
+        },
+        {
+          id: "HC35",
+          ruTitle: "Профнастил HC35",
+        },
+        {
+          id: "H75",
+          ruTitle: "Профнастил H75",
+        },
+        {
+          id: "HC44",
+          ruTitle: "Профнастил HC44",
+        },
+      ],
+      selectedBoard: null,
+    };
+  },
+  methods: {
+    selectItem(data) {
+      if (data.type === "roofs") {
+        this.selectedRoof = data.id;
+      } else if (data.type === "boards") {
+        this.selectedBoard = data.id;
+      }
+    },
+  },
 };
 </script>
 
---
+++ src/components/itemSelection.vue
@@ -0,0 +1,74 @@
+<template>
+  <h2
+    class="text-xl font-bold text-gray-700 border-b border-gray-200 pb-3 mb-8"
+  >
+    {{ section }}
+  </h2>
+  <div
+    class="
+      grid grid-cols-1 grid-cols-1
+      sm:grid-cols-2
+      md:grid-cols-3
+      lg:grid-cols-4
+      xl:grid-cols-5
+      gap-5
+      mb-5
+    "
+  >
+    <div
+      v-for="item in sectionData"
+      :key="item.name"
+      class="flex cursor-pointer border-2 border-white rounded-md"
+      :class="{
+        'border-gray-300': selected === item.id,
+      }"
+      @click="selectItem(item.id)"
+    >
+      <img
+        :src="require(`../assets/images/${item.id}-select.jpg`)"
+        :alt="item.ruTitle"
+        width="90"
+        class="rounded-md m-2 mr-4 border border-gray-100"
+      />
+      <span class="self-center text-sm">{{ item.ruTitle }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "itemSelection",
+  props: {
+    section: {
+      type: String,
+      required: true,
+      default: "",
+    },
+    type: {
+      type: String,
+      required: true,
+      default: "",
+    },
+    sectionData: {
+      type: Array,
+      required: true,
+    },
+  },
+  emits: {
+    selectItem: null,
+  },
+  data() {
+    return {
+      selected: null,
+    };
+  },
+  methods: {
+    selectItem(id) {
+      this.selected = id;
+      this.$emit("selectItem", { type: this.type, id: id });
+    },
+  },
+};
+</script>
+
+<style scoped></style>
